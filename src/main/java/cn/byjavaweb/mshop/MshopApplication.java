05adaef757cd1adec7e42cb22329d1bab6a25cc3
baizhi958216 <1475289190@qq.com>
2023-04-29 22:15:31 +0800

    feat: 初始化SpringBoot项目
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/main/java/cn/byjavaweb/mshop/MshopApplication.java
@@ -0,0 +1,13 @@
+package cn.byjavaweb.mshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MshopApplication.class, args);
+    }
+
+}
