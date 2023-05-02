2b9729d7f07af66bf1cc46d6fe7d4cd2d7c2ca12
baizhi958216 <1475289190@qq.com>
2023-05-02 09:12:39 +0800

    feat(controller): Controller -> RestController
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main/java/cn/byjavaweb/mshop/controller/IndexController.java
+++ src/main/java/cn/byjavaweb/mshop/controller/IndexController.java
@@ -4,15 +4,15 @@ import cn.byjavaweb.mshop.service.TopService;
 import cn.byjavaweb.mshop.utils.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/index")
 public class IndexController {
