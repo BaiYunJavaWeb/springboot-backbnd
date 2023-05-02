cbfe6b150d687440c07530ce3d1a6816b3cdabd1
baizhi958216 <1475289190@qq.com>
2023-05-02 09:08:29 +0800

    feat(接口): 首页推荐
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/main/java/cn/byjavaweb/mshop/controller/IndexController.java
@@ -0,0 +1,38 @@
+package cn.byjavaweb.mshop.controller;
+
+import cn.byjavaweb.mshop.service.TopService;
+import cn.byjavaweb.mshop.utils.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/index")
+public class IndexController {
+    private static final int rows = 16; // 默认每页数量
+
+    private final TopService topService;
+
+    public IndexController(TopService topService){
+        this.topService = topService;
+    }
+
+    /**
+     * 首页推荐 banner 热销 新品
+     */
+    @GetMapping("/index")
+    public ResponseEntity<String> index(){
+        Map<String, Object> msgMap = new HashMap<>();
+        msgMap.put("ibanner", topService.getList(1, 1, 1));
+        msgMap.put("ihot", topService.getList(2, 1, 6));
+        msgMap.put("inew", topService.getList(3, 1, 8));
+        return new ResponseUtil().response(msgMap, HttpStatus.OK);
+    }
+}
