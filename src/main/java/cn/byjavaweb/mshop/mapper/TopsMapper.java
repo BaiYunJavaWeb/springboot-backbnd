045be5cfe1804373ab1d85cb396cad2b78caab38
baizhi958216 <1475289190@qq.com>
2023-05-02 09:00:28 +0800

    feat: 新增商品推荐
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main/java/cn/byjavaweb/mshop/mapper/TopsMapper.java
+++ src/main/java/cn/byjavaweb/mshop/mapper/TopsMapper.java
@@ -3,7 +3,13 @@ package cn.byjavaweb.mshop.mapper;
 import cn.byjavaweb.mshop.entity.Tops;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface TopsMapper  extends BaseMapper<Tops> {
+    @Select("select * from tops where type=#{type} order by id desc limit #{begin}, #{size}")
+    List<Tops> getList(@Param("type")int type, @Param("begin")int begin, @Param("size")int size);
 }
