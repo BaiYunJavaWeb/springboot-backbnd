cb31ec8be496506121f59d54784b45fe2cc789ba
baizhi958216 <1475289190@qq.com>
2023-05-05 14:21:46 +0800

    feat: 类目接口
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main/java/cn/byjavaweb/mshop/controller/TypeController.java
+++ src/main/java/cn/byjavaweb/mshop/controller/TypeController.java
@@ -1,13 +1,11 @@
 package cn.byjavaweb.mshop.controller;
 
+import cn.byjavaweb.mshop.entity.Types;
 import cn.byjavaweb.mshop.service.TypeService;
 import cn.byjavaweb.mshop.utils.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,52 +28,32 @@ public class TypeController {
         msgMap.put("typeList",typeService.getList());
         return new ResponseUtil().response(msgMap, HttpStatus.OK);
     }
-//
-//    /**
-//     * 类目添加
-//     *
-//     * @return
-//     */
-//    @RequestMapping("/typeSave")
-//    public String typeSave(Types type,
-//                           @RequestParam(required=false, defaultValue="1") int page) {
-//        typeService.add(type);
-//        return "redirect:typeList?flag=4&page="+page;
-//    }
-//
-//    /**
-//     * 类目更新
-//     *
-//     * @return
-//     */
-//    @RequestMapping("/typeEdit")
-//    public String typeUp(int id, HttpServletRequest request) {
-//        request.setAttribute("flag", 4);
-//        request.setAttribute("type", typeService.get(id));
-//        return "/admin/type_edit.jsp";
-//    }
-//
-//    /**
-//     * 类目更新
-//     *
-//     * @return
-//     */
-//    @RequestMapping("/typeUpdate")
-//    public String typeUpdate(Types type,
-//                             @RequestParam(required=false, defaultValue="1") int page) {
-//        typeService.update(type);
-//        return "redirect:typeList?flag=4&page="+page;
-//    }
-//
-//    /**
-//     * 类目删除
-//     *
-//     * @return
-//     */
-//    @RequestMapping("/typeDelete")
-//    public String typeDelete(Types type,
-//                             @RequestParam(required=false, defaultValue="1") int page) {
-//        typeService.delete(type);
-//        return "redirect:typeList?flag=4&page="+page;
-//    }
+
+    /**
+     * 类目添加
+     */
+    @PostMapping("/typeSave")
+    public ResponseEntity<String> typeSave(@RequestBody Types types) {
+        typeService.add(types);
+        return new ResponseUtil().response("类目添加成功!", HttpStatus.OK);
+    }
+
+    /**
+     * 类目更新
+     */
+    @PutMapping("/typeUpdate")
+    public ResponseEntity<String> typeUpdate(@RequestBody Types types) {
+        typeService.update(types);
+        return new ResponseUtil().response("类目更新成功!", HttpStatus.OK);
+    }
+
+    /**
+     * 类目删除
+     */
+    @DeleteMapping("/typeDelete")
+    public ResponseEntity<String> typeDelete(@RequestBody Types types) {
+        Map<String, Object> msgMap = new HashMap<>();
+        msgMap.put("success",typeService.delete(types));
+        return new ResponseUtil().response(msgMap,HttpStatus.OK);
+    }
 }
